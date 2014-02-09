2075b429efdd72db979b78cf1d1775d9170cf8d5
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-02-09 03:27:22 -0600

    made changes

--- clarity.js
+++ clarity.js
@@ -3,35 +3,47 @@
 function get_cam(top, bottom, left, right) {
    var url = "https://api.tomtom.com/trafficcams/boxquery";
    var data = {
-	 top: "40.317232",
-	 bottom: "39.123123",
-	 left: "-86.037083",
-	 right: "-85.037083",
+	 top: top,
+	 bottom: bottom,
+	 left: left,
+	 right: right,
 	 format: "json",
 	 key: "fmma8ptu3fgv2cu66uzs3v2a"
    };
    
-   $.get(url, data, function(data) {
-		var id = data.cameras[0].cameraId;
+   $.get(url, data, function(response) {
+	   if (response.cameras[0])
+	   {
+		var id = response.cameras[0].cameraId;
 		//var cameras = new Array();
 		//var cameras_ids = new Array();
-		data.cameras.forEach(function(camera) {
+		response.cameras.forEach(function(camera) {
 			//cameras.push(camera.cameraName);
 			//cameras_ids.push(camera.cameraId);
 			$('#camera_names')
 			  .append($('<option>', { value : camera.cameraId })
 			  .text(camera.cameraName));	
 		});
-		
-   }, 'json');
+	   }
+   }, 'jsonp');
 }
 
 function get_request() {
-	var url = "https://api.tomtom.com/lbs/geocoding/geocode?L=Kalamazoo&AA=Michigan&format=json&key=x4gqmhmn2wr2pgnauxnzb3zd";
-
-	$.get(url, function(response) {
-		
-	}, 'xml');
+	var url = "https://api.tomtom.com/lbs/geocoding/geocode";
+	var data = {
+		L: $('#city').val(),
+		AA: $('#state').val(),
+		format: 'jsonp',
+		key: 'x4gqmhmn2wr2pgnauxnzb3zd'
+	};
+	
+	$.get(url, data, function(response) {
+		var lat = response.geoResponse.geoResult[0].latitude;
+		var long = response.geoResponse.geoResult[0].longitude;
+		console.log(lat);
+		console.log(long);
+		get_cam(lat + 1, lat - 1, long - 1, long + 1);
+	}, 'jsonp');
 }
 
 function view_cam()
